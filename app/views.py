1517aad206926c28b096116ca48f61f2f3324140
imadityacoder <iamadityacoder01@gmail.com>
2023-09-16 18:00:50 +0530

    form completed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from app.models import Form
 
 # Create your views here.
-def index(request):
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        form = Form(username=username,email=email,password=password)
+        form.save()
+        
+
     return render(request,"index.html")
